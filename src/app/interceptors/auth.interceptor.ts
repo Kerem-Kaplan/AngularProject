1c42b9511f93dbd319c2af1d21d4e42576284912
Kerem-Kaplan <keremkaplan1000@gmail.com>
2022-11-28 12:38:56 +0300

    Final

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -18,6 +18,6 @@ export class AuthInterceptor implements HttpInterceptor {
     newRequest=request.clone({
       headers:request.headers.set("Authorization","Bearer " +  token)
     })
-    return next.handle(newRequest);
+    return next.handle(newRequest); 
   }
 }
